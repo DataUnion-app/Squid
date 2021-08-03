7fa29222bdb4cb7bab92410de9952c82e4852295
intel9911 <tiop08317@gmail.com>
2021-08-02 23:28:40 -0400

    Added algorithm tag

--- src/component.js
+++ src/component.js
@@ -2,6 +2,9 @@ import Vue from 'vue';
 import CImage from '@/components/CImage';
 import CHeader from "@/components/CHeader";
 import CPopMenu from "@/components/CPopMenu";
+import CArticleCard from "@/components/CArticleCard";
+
 Vue.component('CImage', CImage);
 Vue.component('CHeader', CHeader);
 Vue.component('CPopMenu', CPopMenu);
+Vue.component('CArticleCard', CArticleCard);
