91aca2a1369f53e68e9af4876e2ebb429002b78a
Sarah Kay <51705696+SarahKay99@users.noreply.github.com>
2021-09-10 15:38:12 +1000

    change

---
+++ src/views/Loading.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="smt-spinner"></div>
+</template>
+
+<script>
+
+export default {
+  name: 'Loading',
+  props: {
+  },
+  watch: {
+  },
+  data() {
+    return {
+    };
+  },
+  components: {
+  },
+  computed: {
+  },
+  methods: {
+  },
+  mounted() {
+  }
+}
+</script>
