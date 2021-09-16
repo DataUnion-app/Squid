c4a7eed546290e605b6f3ff8df882b6f7c813d87
Sarah Kay <51705696+SarahKay99@users.noreply.github.com>
2021-09-16 20:01:17 +1000

    changes

--- src/components/CMetamaskPopup.vue
+++ src/components/CMetamaskPopup.vue
@@ -9,7 +9,7 @@
         </template>
         
         <div v-if="!loggedIn" class="wallet-content">
-            <i>(These features are coming soon. Please use the Metamask widget to log in and out, and get help through the About section for now)</i>
+            <i>(These features are coming soon. Please use the external Metamask widget to log in and out, and get help through the About section for now)</i>
             <button @click="connectWallet">Connect Wallet</button>
             <button @click="redirectToDownload">Download Metamask</button>
         </div>
@@ -33,7 +33,7 @@
     padding: 0 !important;
   }
 
-  .vs-dialog {
+  .overwrite .vs-dialog {
     background-color: RGB(244, 247, 248) !important;
     margin-bottom: none !important;
     border-radius: 0px !important;
@@ -41,18 +41,20 @@
     max-width: 250px !important;        /* var(--sidebar-width) */
     opacity: 55%;
     margin: none !important;
-  }
-
-  .vs-dialog-content {
     top: 30% !important;
     left: 5px !important;
   }
 
-  .vs-dialog__header {
+  .overwrite .vs-dialog .vs-dialog-content {
+    top: 30% !important;
+    left: 5px !important;
+  }
+
+  .overwrite .vs-dialog .vs-dialog__header {
     padding: 2% !important;
   }
 
-  .vs-dialog__content {
+  .overwrite .vs-dialog .vs-dialog__content {
     padding: 0px !important;
     margin-bottom: 0px !important;
     top: 30% !important;
